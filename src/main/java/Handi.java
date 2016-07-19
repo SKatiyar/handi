8ba408268dfdbaa72f684bed566bc1356943c067
Suyash Katiyar <katiyar.suyash@gmail.com>
2016-07-20 01:42:59 +0530

    floyd

--- src/main/java/Handi.java
+++ src/main/java/Handi.java
@@ -3,11 +3,9 @@ import static spark.Spark.*;
 public class Handi {
     public static void main(String[] args) {
         // Define heroku port
-        Integer herokuPort = Integer.valueOf(System.getenv("PORT"));
-        herokuPort =  (herokuPort > 0) ? herokuPort : Integer.valueOf(5000);
-        port(herokuPort);
+        port((System.getenv("PORT") != "") ? Integer.valueOf(System.getenv("PORT")) : Integer.valueOf(5000));
 
 
-        get("/", (req, res) -> "Hello World");
+        get("/", (req, res) -> " If you didn't care what happened to me, <br> And I didn't care for you, <br> We would zig zag our way through the boredom and pain <br> Occasionally glancing up through the rain. <br> Wondering which of the buggars to blame <br> And watching for pigs on the wing.");
     }
 }
