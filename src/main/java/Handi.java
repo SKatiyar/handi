21837ec41f8ec6e974141b235807e85d0453d5b8
Suyash Katiyar <katiyar.suyash@gmail.com>
2016-07-19 13:15:31 +0530

    pick port from runtime in heroku

--- src/main/java/Handi.java
+++ src/main/java/Handi.java
@@ -2,6 +2,12 @@ import static spark.Spark.*;
 
 public class Handi {
     public static void main(String[] args) {
+        // Define heroku port
+        Integer herokuPort = Integer.valueOf(System.getenv("PORT"));
+        herokuPort =  (herokuPort > 0) ? herokuPort : Integer.valueOf(5000);
+        port(herokuPort);
+
+
         get("/", (req, res) -> "Hello World");
     }
 }
